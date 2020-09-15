1a6da5ab4ac82054ee88818c978513152f6df901
JAlonso1977 <69387552+JAlonso1977@users.noreply.github.com>
2020-09-15 13:44:10 +0200

    Add files via upload

---
+++ js/visualiza.js
@@ -0,0 +1,44 @@
+/*Name this external file gallery.js*/
+/*Jesús Alonso Week 2 assingment */
+
+function inicia(){
+    document.getElementById('salud').style.display="block";
+	}
+
+  function salud(){
+      document.getElementById('calidad').style.display="block";
+  	}
+
+  function calidad(){
+      document.getElementById('breeam0').style.display="block";
+    }
+  function breeamSi(){
+        document.getElementById('breeam').style.display="block";
+    	}
+  function breeamNo(){
+        document.getElementById('bim0').style.display="block";
+      }
+  function bimSi(){
+        document.getElementById('bim').style.display="block";
+      }
+  function bimNo(){
+        document.getElementById('lean0').style.display="block";
+      }
+function leanSi(){
+      document.getElementById('lean').style.display="block";
+      }
+function leanNo(){
+      document.getElementById('aemet0').style.display="block";
+      }
+function aemetSi(){
+      document.getElementById('aemet').style.display="block";
+      }
+function aemetNo(){
+      document.getElementById('perso0').style.display="block";
+      }
+function persoSi(){
+      document.getElementById('perso').style.display="block";
+      }
+function persoNo(){
+      document.getElementById('fin').style.display="block";
+      }
