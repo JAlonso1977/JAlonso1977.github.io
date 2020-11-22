aecbe672298f0fe8a451156f2b8dac963b136d03
JAlonso1977 <69387552+JAlonso1977@users.noreply.github.com>
2020-11-22 22:53:30 +0100

    Add files via upload

--- js/visualiza.js
+++ js/visualiza.js
@@ -2,43 +2,79 @@
 /*Jesús Alonso Week 2 assingment */
 
 function inicia(){
-    document.getElementById('salud').style.display="block";
+    document.getElementById('Pregunta1').style.display="block";
 	}
 
-  function salud(){
-      document.getElementById('calidad').style.display="block";
+  function pregunta1(){
+      document.getElementById('Pregunta1_2').style.display="block";
   	}
 
-  function calidad(){
-      document.getElementById('breeam0').style.display="block";
+  function pregunta1_2(){
+      document.getElementById('Pregunta2').style.display="block";
     }
-  function breeamSi(){
-        document.getElementById('breeam').style.display="block";
+
+  function pregunta2(){
+        document.getElementById('Pregunta2_2').style.display="block";
     	}
-  function breeamNo(){
-        document.getElementById('bim0').style.display="block";
+
+  function pregunta2_2(){
+        document.getElementById('Pregunta3').style.display="block";
       }
-  function bimSi(){
-        document.getElementById('bim').style.display="block";
+
+  function pregunta3(){
+            document.getElementById('Pregunta3_2').style.display="block";
+        	}
+
+  function pregunta3_2(){
+            document.getElementById('Pregunta4').style.display="block";
+          }
+  function pregunta4(){
+            document.getElementById('Pregunta4_2').style.display="block";
+      	}
+
+  function pregunta4_2(){
+            document.getElementById('Pregunta5').style.display="block";
       }
-  function bimNo(){
-        document.getElementById('lean0').style.display="block";
-      }
-function leanSi(){
-      document.getElementById('lean').style.display="block";
-      }
-function leanNo(){
-      document.getElementById('aemet0').style.display="block";
-      }
-function aemetSi(){
-      document.getElementById('aemet').style.display="block";
-      }
-function aemetNo(){
-      document.getElementById('perso0').style.display="block";
-      }
-function persoSi(){
-      document.getElementById('perso').style.display="block";
-      }
-function persoNo(){
-      document.getElementById('fin').style.display="block";
-      }
+      function pregunta5(){
+                document.getElementById('Pregunta5_2').style.display="block";
+          	}
+
+      function pregunta5_2(){
+                document.getElementById('Pregunta6').style.display="block";
+          }
+      function pregunta6(){
+                document.getElementById('Pregunta6_2').style.display="block";
+          	}
+
+      function pregunta6_2(){
+                document.getElementById('Pregunta7').style.display="block";
+          }
+      function pregunta7(){
+                document.getElementById('Pregunta7_2').style.display="block";
+          	}
+
+      function pregunta7_2(){
+                document.getElementById('Pregunta8').style.display="block";
+          }
+      function pregunta8(){
+                document.getElementById('Pregunta8_2').style.display="block";
+          	}
+
+      function pregunta8_2(){
+                document.getElementById('Pregunta9').style.display="block";
+          }
+      function pregunta9(){
+                document.getElementById('Pregunta9_2').style.display="block";
+          	}
+
+      function pregunta9_2(){
+                document.getElementById('Pregunta10').style.display="block";
+          }
+
+      function pregunta10(){
+                document.getElementById('Pregunta10_2').style.display="block";
+          	}
+
+      function pregunta10_2(){
+                document.getElementById('Final').style.display="block";
+          }
